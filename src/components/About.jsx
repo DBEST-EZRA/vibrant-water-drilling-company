baef0aabebb38c1bdafe9f7c8a8968121c01f156
DBEST-EZRA <ezradbest101@gmail.com>
2025-01-20 17:40:27 +0300

    sixteen commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./about.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <section className="about">
+        <div className="container flex mtop">
+          <div className="left row">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip.
+            </p>
+            <button className="btn2">More About Us</button>
+          </div>
+          <div className="right row">
+            <img src="./immio.jpg" alt="" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default About;
