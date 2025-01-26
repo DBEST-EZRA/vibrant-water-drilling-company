2df134aff5c4b13bb09efa0b8d7993eea27faba0
DBEST-EZRA <ezradbest101@gmail.com>
2025-01-26 23:31:53 +0300

    added admin dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+
+  //   content of the Dashboard
+  // Progress cards with action buttons (edit, delete)
+  // A single add button at the top
+  // View new orders
+};
+
+export default Dashboard;
