4c424827d38ad092bff1f615273067be0ae7a8db
DBEST-EZRA <ezradbest101@gmail.com>
2025-01-27 12:33:31 +0300

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,29 +9,36 @@ const Header = () => {
   };
 
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">Vibrant Water Drilling</div>
-      <button className="menu-toggle" onClick={toggleMenu}>
-        <span className="menu-icon">☰</span>
-      </button>
-      <ul className={`navbar-links ${isOpen ? "open" : ""}`}>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="about">About</a>
-        </li>
-        <li>
-          <a href="services">Services</a>
-        </li>
-        <li>
-          <a href="contact">Contact</a>
-        </li>
-        <li>
-          <a href="track-progress">Track Progress</a>
-        </li>
-      </ul>
-    </nav>
+    <div className="containerB">
+      <nav className="navbar">
+        <div className="navbar-logo">Vibrant Water Drilling</div>
+        <button
+          className={`menu-toggle ${isOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span className={`menu-icon ${isOpen ? "open" : ""}`}>
+            {isOpen ? "✖" : "☰"}
+          </span>
+        </button>
+        <ul className={`navbar-links ${isOpen ? "open" : ""}`}>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="about">About</a>
+          </li>
+          <li>
+            <a href="services">Services</a>
+          </li>
+          <li>
+            <a href="contact">Contact</a>
+          </li>
+          <li>
+            <a href="track-progress">Track Progress</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
